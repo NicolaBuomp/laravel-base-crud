e18ec1cceaa1d042547145c4b34366351da7e86b
Nicola <niconix.dev@gmail.com>
2020-06-17 15:26:17 +0200

    Stucture

---
+++ laravel-base-crud/resources/views/partials/header.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css'/>
+    <title>Laravel Crud NBA</title>
+</head>
+<body>
+    <header>
+        <nav class="navbar navbar-expand navbar-light bg-light">
+            <div class="nav-brand">
+                <img src="https://upload.wikimedia.org/wikipedia/it/thumb/e/e1/Nba1.png/140px-Nba1.png" style="width: 40px" alt="Logo">
+            </div>
+            
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="">Teams</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="">Info</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
